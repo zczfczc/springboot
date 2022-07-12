e47d49fc7154cac4ff253ed0c5bbc6fd266f6c93
zfc <1655693051@qq.com>
2022-07-12 19:52:33 +0800

    优化

--- src/main/java/cn/zfc/Application.java
+++ src/main/java/cn/zfc/Application.java
@@ -1,7 +1,6 @@
 package cn.zfc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @Slf4j
 @SpringBootApplication
-@MapperScan("cn.zfc.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
